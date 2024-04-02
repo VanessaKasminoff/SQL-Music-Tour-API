b40dfb62004da2afb9fb36a3e939703550c6f9f7
Vanessa Kasminoff <sunnset07@gmail.com>
2024-04-01 22:03:19 -0700

    Generated model with sequelize

---
+++ models/artists.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Artists extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Artists.init({
+    artist_id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    genre: DataTypes.TEXT,
+    available_start_time: DataTypes.DATE,
+    end_time: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Artists',
+    tableName: 'artists',
+    timestamps: false
+  });
+  return Artists;
+};
