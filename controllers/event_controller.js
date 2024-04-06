82dd7c595fab7a71484202cc2f4f49cbacd69b17
Vanessa Kasminoff <sunnset07@gmail.com>
2024-04-05 23:46:17 -0700

    Finished event controller

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -25,11 +25,55 @@ events.get('/:id', async (req, res) => {
         const foundEvent = await Event.findOne({
             where: {event_id: req.params.id}
         })
+        res.status(200).json(foundEvent)
     } catch (error) {
         res.status(500).json(error)
     }
 })
 
-// CREATE AN EVENT
+//CREATE AN EVENT
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new event',
+            data: newEvent
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//UPDATE AN EVENT
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvents = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvents} event(s)`
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//DELETE AN EVENT
+events.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvents = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvents} event(s)`
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = events
